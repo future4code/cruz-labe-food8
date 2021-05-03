33d064a48342ca44c2b1b48ea3486ab1091eeaea
Nicolas <nicolasalexandre0001@gmail.com>
2021-05-03 12:29:36 -0300

    add basic pages

--- src/GlobalState/GlobalStateProvider.js
+++ src/GlobalState/GlobalStateProvider.js
@@ -1,35 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
-import GlobalStateContext from "../Global/GlobalStateContext";
-import { baseUrl, axiosConfig } from '../Constants/api'
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import GlobalStateContext from "./GlobalStateContext";
+import axios from "axios";
+import { goToLogin } from "../Router/coordinator";
 
 const GlobalStateProvider = (props) => {
-   
-    useEffect(() => {
-       
-    }, [])
+  const history = useHistory();
+  useEffect(() => {}, []);
 
-    const logout = () => {
-        window.localStorage.removeItem("token");
-        goToLogin(history);
-    };
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    goToLogin(history);
+  };
 
+  const states = {};
+  const setters = {};
+  const requests = {};
 
-    const states = {  };
-    const setters = {  };
-    const requests = {  };
+  const data = { states, setters, requests };
 
-    const data = { states, setters, requests };
-
-    return (
-        <div>
-            <GlobalStateContext.Provider value={data}>
-                {props.children}
-            </GlobalStateContext.Provider>
-        </div>
-    );
+  return (
+    <div>
+      <GlobalStateContext.Provider value={data}>
+        {props.children}
+      </GlobalStateContext.Provider>
+    </div>
+  );
 };
 
-export default GlobalStateProvider; 
+export default GlobalStateProvider;
