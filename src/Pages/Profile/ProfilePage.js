c95698d1c7274444d078dda07f8cff22a6b6848b
Isabelle Frederico Travasso <isa.frederico@gmail.com>
2021-05-09 01:53:13 -0300

    Com estilo

--- src/Pages/Profile/ProfilePage.js
+++ src/Pages/Profile/ProfilePage.js
@@ -7,13 +7,15 @@ import GlobalStateContext from "../../GlobalState/GlobalStateContext";
 import useRequestData from '../../Hooks/useRequestData'
 import EditIcon from '@material-ui/icons/Edit';
 import * as S from "./Styled"
-import { IconButton } from "@material-ui/core";
+import { IconButton, Typography } from "@material-ui/core";
+import { useStyles } from './Styled'
 
 import Loading from '../../Components/Loading/Loading'
 
 
 function ProfilePage() {
     useProtectedPage()
+    const classes = useStyles();
     const history = useHistory();
     const { states } = useContext(GlobalStateContext);
     const profile = useRequestData({}, `${baseUrl}/profile/`)
@@ -26,16 +28,33 @@ function ProfilePage() {
 
     }, [address, profile]);
 
+    const renderPrice = (price) => {
+        if (price % 1 === 0) {
+            return (
+                <S.Text bold>
+                    SUBTOTAL R${price}.00
+                </S.Text>
+            )
+        } else {
+            return (
+                <S.Text bold>
+                   SUBTOTAL R${price}0
+                </S.Text>
+            )
+        }
+    }
+
     const listOrders =
         arrayOrders ? (
             arrayOrders.map((order) => {
+                let date = new Date(order.createdAt)
+                const options = { year: 'numeric', month: 'long', day: 'numeric' };
                 return (
-                    <div>
-                        <hr />
-                        <p>{order.restaurantName}</p>
-                        <p>Valor Total: R${order.totalPrice}</p>
-                        <hr />
-                    </div>
+                    <S.CardOrder>
+                        <Typography className={classes.title} color='primary' variant="p" component="p">{order.restaurantName} </Typography>
+                        <S.Date>{date.toLocaleDateString('pt-BR', options)}</S.Date>
+                        {renderPrice(order.totalPrice)}
+                    </S.CardOrder>
                 )
             })
         ) : (<Loading />)
@@ -43,16 +62,14 @@ function ProfilePage() {
     return (
         <S.MainContainer>
             <S.Header>
-                <S.Title>Meu Perfil</S.Title>
-                <hr />
+                <S.HeaderTitle>Meu Perfil</S.HeaderTitle>
             </S.Header>
             <S.UserContainer>
                 {user ? (
-
                     <S.UserData>
-                        <p>{user.name}</p>
-                        <p>{user.email}</p>
-                        <p>{user.cpf}</p>
+                        <S.Text>{user.name}</S.Text>
+                        <S.Text>{user.email}</S.Text>
+                        <S.Text>{user.cpf}</S.Text>
                     </S.UserData>)
                     : (<Loading />)}
                 <IconButton onClick={() => goToProfileNameEdit(history)}>
@@ -62,10 +79,10 @@ function ProfilePage() {
 
             <S.AddressContainer>
                 <S.AddressData>
-                    <h4>Endereço Cadastrado</h4>
+                <Typography className={classes.information} component="p"> Endereço Cadastrado </Typography>
                     {address ? (
                         <div>
-                            <p>{address.street},  {address.number}  - {address.neighbourhood}</p>
+                            <S.Text>{address.street},  {address.number}  - {address.neighbourhood}</S.Text>
                         </div>
                     ) : (<Loading />)}
                 </S.AddressData>
@@ -76,7 +93,7 @@ function ProfilePage() {
             </S.AddressContainer>
 
             <S.HistoryContainer>
-                <p>Histórico de Pedidos</p>
+            <Typography className={classes.historyTitle} component="p"> Histórico de Pedidos </Typography>
                 <S.ListOrders> {listOrders} </S.ListOrders>
             </S.HistoryContainer>
         </S.MainContainer>
